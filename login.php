a597993e4cc62e3cdd2e2ec7c4bd93daa6f7e34c
rawrhel <noviirahell@students.unnes.ac.id>
2025-10-17 16:31:52 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php 
+session_start(); 
+
+$username_valid = "unnes"; 
+$password_valid = "1234"; 
+
+if(!isset($_POST["username"]) || !isset($_POST["password"])) {
+    header("Location: index.html");
+}
+
+$username = $_POST["username"]; 
+$password = $_POST["password"]; 
+
+if ($username == $username_valid && $password == $password_valid) { 
+    $_SESSION['login'][] = [ 
+        'username' => $username, 
+        'login_at' => date('Y-m-d H:i:s') 
+    ]; 
+
+    // jika benar 
+    echo "Selamat Datang: " . $username . ", anda telah login sebanyak: " . count($_SESSION['login']) . " kali"; 
+    echo '<br>'; 
+    echo '<a href="logout.php">Logout</a>'; 
+    echo '<pre>';
+    var_dump($_SESSION['login']); 
+} else { 
+    // jika salah 
+    echo "Username atau password salah!"; 
+}
+?>
